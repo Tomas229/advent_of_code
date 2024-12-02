c8e7dcd57aab12aaa9d6dbf9335efc819c4120d0
Tomas C <tomas.cortez@idok.cl>
2024-12-02 00:14:59 -0300

    feat(2024-1): Obtiene solucion dia 1 parte 2

--- challenges/2024/01/solution.rb
+++ challenges/2024/01/solution.rb
@@ -5,22 +5,20 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      left = []
-      right = []
-      data.each do |d|
-        l,r = d.split
-        left += [l.to_i]
-        right += [r.to_i]
-      end
-      left.sort!
-      right.sort!
-
-      distance = left.zip(right).map { |x| x.reduce(:-).abs }
+      custom_process(data)
+      distance = @left.zip(@right).map { |x| x.reduce(:-).abs }
       distance.reduce(:+)
     end
 
     def part_2
-      nil
+      custom_process(data)
+      freq_hash = @right.group_by { |a| a }
+      freq_hash.transform_values! { |v| v.size}
+      res = 0
+      @left.each do |n|
+        res += (freq_hash[n] || 0) * n
+      end
+      res
     end
 
     private
@@ -33,5 +31,16 @@ module Year2024
     # def process_dataset(set)
     #   set
     # end
+    def custom_process(data)
+      @left = []
+      @right = []
+      data.each do |d|
+        l,r = d.split
+        @left += [l.to_i]
+        @right += [r.to_i]
+      end
+      @left.sort!
+      @right.sort!
+    end
   end
 end
