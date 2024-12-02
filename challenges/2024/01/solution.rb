9d3ed9f6dba66c067c088aa1e2e183a5f0689cfb
Tomas C <tomas.cortez@idok.cl>
2024-12-02 00:18:05 -0300

    style: Cambios para adherirse a convenciones ruby

--- challenges/2024/01/solution.rb
+++ challenges/2024/01/solution.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
+
 module Year2024
+  # Solution
   class Day01 < Solution
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
@@ -13,7 +15,7 @@ module Year2024
     def part_2
       custom_process(data)
       freq_hash = @right.group_by { |a| a }
-      freq_hash.transform_values! { |v| v.size}
+      freq_hash.transform_values!(&:size)
       res = 0
       @left.each do |n|
         res += (freq_hash[n] || 0) * n
@@ -22,6 +24,7 @@ module Year2024
     end
 
     private
+
     # Processes each line of the input file and stores the result in the dataset
     # def process_input(line)
     #   line.map(&:to_i)
@@ -35,7 +38,7 @@ module Year2024
       @left = []
       @right = []
       data.each do |d|
-        l,r = d.split
+        l, r = d.split
         @left += [l.to_i]
         @right += [r.to_i]
       end
--- spec/2024/01/solution_spec.rb
+++ spec/2024/01/solution_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe Year2024::Day01 do
-  let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2024/01/input.txt")) }
-  let(:example_input) {
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/01/input.txt')) }
+  let(:example_input) do
     <<~EOF
       3   4
       4   3
@@ -12,25 +13,25 @@ RSpec.describe Year2024::Day01 do
       3   9
       3   3
     EOF
-  }
+  end
 
-  describe "part 1" do
-    it "returns 11 for the example input" do
+  describe 'part 1' do
+    it 'returns 11 for the example input' do
       expect(described_class.part_1(example_input)).to eq(11)
     end
 
-    it "returns 3508942 for my input" do
-      expect(described_class.part_1(input)).to eq(3508942)
+    it 'returns 3508942 for my input' do
+      expect(described_class.part_1(input)).to eq(3_508_942)
     end
   end
 
-  describe "part 2" do
-    it "returns 31 for the example input" do
+  describe 'part 2' do
+    it 'returns 31 for the example input' do
       expect(described_class.part_2(example_input)).to eq(31)
     end
 
-    it "returns 26593248 for my input" do
-      expect(described_class.part_2(input)).to eq(26593248)
+    it 'returns 26593248 for my input' do
+      expect(described_class.part_2(input)).to eq(26_593_248)
     end
   end
 end
